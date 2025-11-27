1c97105bf189306e98ee418f45c46c5060acfb2e
Ahmad Salihu <ahmadsalihu2448@gmail.com>
2025-11-27 17:53:29 +0000

    Finished with simple linear regression, I fixed a small bug and added a graph for comparison to see how well my algorithm works sompared to the scikit learn LinearRegression model

--- Simple-Linear-Regression/simple_linear_regression.py
+++ Simple-Linear-Regression/simple_linear_regression.py
@@ -1,16 +1,20 @@
 #Creating the functions to calculate loss and gradient for both m and c values
 def gradientM(m,c,data_x, data_y):
     n = len(data_x)
+    total = 0
     for i in range(n):
         loss = (data_x[i] * ((m*data_x[i] + c) - data_y[i]))
-    gradient_m = (-2/n) * loss
+        total += loss
+    gradient_m = (2/n) * total
     return gradient_m
     
 def gradientC(m,c,data_x, data_y):
     n = len(data_x)
+    total = 0
     for i in range(n):
         loss = (((m*data_x[i] + c) - data_y[i]))
-    gradient_c = (-2/n) * loss
+        total += loss
+    gradient_c = (2/n) * total
     return gradient_c
 
 
@@ -20,10 +24,12 @@ def step(starting_m, starting_c,data_x, data_y, learning_rate, iterations):
     current_m = starting_m
     current_c = starting_c
     
+    print(f"There should be {iterations} iterations")
+
     for t in range(iterations):
         m_gradient = gradientM(current_m, current_c, data_x,data_y)
-        current_m = current_m - (learning_rate * m_gradient)
         c_gradient = gradientC(current_m, current_c, data_x,data_y)
-        current_c = current_c - (learning_rate * c_gradient)
-
+        current_m = current_m - (learning_rate * m_gradient)
+        current_c = current_c - (learning_rate * c_gradient)   
+    
     return [current_m, current_c]
